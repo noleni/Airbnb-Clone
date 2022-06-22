263ffe5dcdd7adbe1b405091e4f6bb2d31f07c25
Marine Ramillon <marine.ramillon@gmail.com>
2022-06-22 22:43:32 +0200

    working on front

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @bookings = @user.bookings.includes(:user)
+    @my_flats = @user.flats.includes(:user)
+    @my_flats_bookings = @user.user_rented_flats
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :flats
   has_many :bookings
+
+  has_many :user_rented_flats, through: :flats, source: :bookings
 end
